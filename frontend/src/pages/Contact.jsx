b9d8fc091db8de53ccd13f9c8fb8d1604cad533e
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-02 12:33:20 +0530

    add pages and configure routes

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      Contack page
+    </div>
+  )
+}
+
+export default Contact
---
+++ frontend/src/pages/MyAppointments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyAppointments = () => {
+  return (
+    <div>
+      My Appointments page
+    </div>
+  )
+}
+
+export default MyAppointments
