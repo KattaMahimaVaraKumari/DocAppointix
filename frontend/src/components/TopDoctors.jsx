5b793ecb7b530c00f4550f6c5e8982e8761a5967
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-04 00:13:56 +0530

    add top doctors component

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { doctors } from '../assets/assets'
+
+const TopDoctors = () => {
+  return (
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+      <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+      <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
+      <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+        {doctors.slice(0,10).map((item,index)=>(
+            <div className='border border-[#ADB5BD] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                <img className="bg-[#CED4DA]" src={item.image} alt="" />
+                <div className='p-4'>
+                    <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p>
+                        <p>Available</p>
+                    </div>
+                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                    <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                </div>
+            </div>
+        ))}
+      </div>
+      <button className='bg-[#CED4DA] text-black rounded-xl mt-10 py-3 px-12'>more</button>
+    </div>
+  )
+}
+
+export default TopDoctors
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
     <div>
       <Header/>
       <SpecialityMenu/>
+      <TopDoctors/>
     </div>
   )
 }
