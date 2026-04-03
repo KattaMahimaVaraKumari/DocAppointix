dff54cd2e0723c847db158aac65b31c917045173
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-03 18:38:01 +0530

    Add  header

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -10,7 +10,6 @@ import menu_icon from './menu_icon.svg'
 import cross_icon from './cross_icon.png'
 import chats_icon from './chats_icon.svg'
 import verified_icon from './verified_icon.svg'
-import arrow_icon from './arrow_icon.svg'
 import info_icon from './info_icon.svg'
 import upload_icon from './upload_icon.png'
 import stripe_logo from './stripe_logo.png'
@@ -47,7 +46,6 @@ export const assets = {
     verified_icon,
     info_icon,
     profile_pic,
-    arrow_icon,
     contact_image,
     about_image,
     menu_icon,
