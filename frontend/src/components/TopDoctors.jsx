190485b7d5be75ccdf488cb2ebfe521cede302fe
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-04 00:49:28 +0530

    add navigate to appointment page using doctor id

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 import { doctors } from '../assets/assets'
 
 const TopDoctors = () => {
+  const navigate=useNavigate()
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
       <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {doctors.slice(0,10).map((item,index)=>(
-            <div className='border border-[#ADB5BD] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+            <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-[#ADB5BD] rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                 <img className="bg-[#CED4DA]" src={item.image} alt="" />
                 <div className='p-4'>
                     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
