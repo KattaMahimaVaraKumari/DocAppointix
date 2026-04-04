ef2b561ccf1638d83738085b09495874bb962341
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-04 11:07:04 +0530

    add AppContextProvider to share doctors data

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {useNavigate} from 'react-router-dom'
-import { doctors } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const TopDoctors = () => {
   const navigate=useNavigate()
+  const {doctors}=useContext(AppContext)
+
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
@@ -23,7 +25,7 @@ const TopDoctors = () => {
             </div>
         ))}
       </div>
-      <button className='bg-[#CED4DA] text-black rounded-xl mt-10 py-3 px-12'>more</button>
+      <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-[#CED4DA] text-black rounded-xl mt-10 py-3 px-12'>more</button>
     </div>
   )
 }
