92b6116e6d7a0e5142abfc81610dfd1d9386d160
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-05 02:30:51 +0530

    update About section content

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -11,10 +11,10 @@ const About = () => {
       <div className='flex flex-cols flex-col md:flex-row gap-12 my-10'>
         <img className='w-full md:max-w-[260px]' src={assets.about_image} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
-          <p>Welcome to DocAppointix, your trusted partner in managing your healthcare needs conveniently and efficiently. At DocAppointix, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records</p>
+          <p>DocAppointix is a full-stack healthcare web application designed to simplify the process of booking doctor appointments and managing healthcare interactions. It provides a seamless and efficient platform for patients to connect with healthcare professionals.</p>
           <p>DocAppointix is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, DocAppointix is here to support you every step of the way.</p>
           <b className='text-gray-800'>Our vision</b>
-          <p> Our vision at DocAppointix is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
+          <p>Our vision is to create a seamless and accessible healthcare experience for everyone. We aim to bridge the gap between patients and healthcare providers, ensuring that quality care is always within reach.</p>
         </div>
       </div>
     </div>
