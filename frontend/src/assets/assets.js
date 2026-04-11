cf510e45106c007c826820006ce5cf1c240ab9b1
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-11 11:45:02 +0530

    add API to fetch all doctors for admin panel

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -202,7 +202,7 @@ export const doctors = [
         image: doc9,
         speciality: 'Dermatologist',
         degree: 'MBBS',
-        experience: '1 Years',
+        experience: '1 Year',
         about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
         fees: 30,
         address: {
