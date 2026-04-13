09f79be84ad9a7172f13ad6b42f7ce8fa7d83fe6
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-14 00:56:34 +0530

    filter out booked time slots when displaying available appointments

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -50,12 +50,25 @@ const Appointment = () => {
 
       let timeSlots=[]
 
-      while(currentDate < endTime){
-        let formattedTime=currentDate.toLocaleTimeString([],{hour:'2-digit',minute:'2-digit'})
-        timeSlots.push({
-          datetime:new Date(currentDate),
-          time:formattedTime
-        })
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+
+        let day = currentDate.getDate();
+        let month = currentDate.getMonth() + 1;
+        let year = currentDate.getFullYear();
+
+        const slotDate = day + "_" + month + "_" + year;
+        const slotTime = formattedTime;
+        
+        if (!docInfo || !docInfo.slots_booked) return;
+        const isSlotAvailable = docInfo.slots_booked[slotDate] && docInfo.slots_booked[slotDate].includes(slotTime) ? false : true;
+
+        if (isSlotAvailable) {
+          timeSlots.push({
+            datetime: new Date(currentDate),
+            time: formattedTime
+          })
+        }
         
         currentDate.setMinutes(currentDate.getMinutes()+30)
       }
