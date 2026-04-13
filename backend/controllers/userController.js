5e6fcfcb44c48cd2dc9dccf5f7f4c0ae59c02af7
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-13 22:23:56 +0530

    add book appointment API with doctor availability and slot management

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken'
 import {v2 as cloudinary} from 'cloudinary'
+import doctorModel from '../models/doctorModel.js';
+import appointmentModel from '../models/appointmentModel.js';
 
 //API to register user
 const registerUser = async (req, res) => {
@@ -105,6 +107,58 @@ const updateProfile = async (req, res) => {
 
         res.json({success:true, message:'Profile Updated'})
 
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+//API to book appointment
+const bookAppointment = async (req,res) =>{
+    try {
+        
+        const {userId, docId, slotDate, slotTime}= req.body;
+
+        const docData = await doctorModel.findById(docId).select('-password');
+
+        if(!docData.available){
+            return res.json({success:false,message:'Doctor not available'});
+        }
+
+        let slots_booked = docData.slots_booked;
+
+        //checking for slot availability
+        if(slots_booked[slotDate]){
+            if(slots_booked[slotDate].includes(slotTime)){
+                 return res.json({success:false,message:'Slot not available'});
+            }
+            else{
+                slots_booked[slotDate].push(slotTime)
+            }
+        }
+        else{
+            slots_booked[slotDate]=[];
+            slots_booked[slotDate].push(slotTime);
+        }
+
+        const userData=await userModel.fmmindById(userId).select('-password');
+
+        delete docData.slots_booked;
+
+        const appointmentData= {
+            userId,
+            docId,
+            uerData,
+            docData,
+            amount:docData.fees,
+            slotTime,
+            slotDate,
+            date:date.now()
+        }
+
+        const newAppointment=new appointmentModel(appointmentData);
+        newAppointment.save()
+        
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message });
