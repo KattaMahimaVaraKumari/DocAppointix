ae5127d7c98d695f2c0b6969dbcbaca6dbe54b37
KattaMahimaVaraKumari <kattamahimavarakumari@gmail.com>
2026-04-14 15:14:53 +0530

    hide actions for cancelled appointments

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 
 const MyAppointments = () => {
   
-  const {backendUrl, token }=useContext(AppContext);
+  const {backendUrl, token, getDoctorsData}=useContext(AppContext);
 
   const [appointments, setAppointments]=useState([]);
 
@@ -37,6 +37,7 @@ const MyAppointments = () => {
       if(data.success){
         toast.success(data.message);
         getUserAppointments();
+        getDoctorsData();
       }
       else{
         toast.error(data.message);
@@ -74,8 +75,9 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
-              <button className='text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-[#495057] hover:text-white transition-all duration-300'>Pay Online</button>
-              <button onClick={()=>cancelAppointment(item._id)} className='text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>
+              {!item.cancelled && <button className='text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-[#495057] hover:text-white transition-all duration-300'>Pay Online</button>}
+              {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-stone-500 text-center sm:min-w-48 py-2 border rounded cursor-pointer hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel appointment</button>}
+              {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 text-red-500 cursor-pointer'>Appointment cancelled</button> }
             </div>
           </div>
         ))}
